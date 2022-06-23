a1d37c3795f4a9bc77cd69698cdf17ad769d6892
OrlyachenkoEvgeny <evgeny.orlyachenko@yandex.ru>
2022-06-23 14:04:54 +0300

    Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,18 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  const newUL = document.createElement('ul'); 
+  document.body.append(newUL);
+
+  const arrNames = friends.map(item => item.firstName + ' ' + item.lastName);
+
+  for (let users of arrNames) {
+    const newLI = document.createElement('LI');
+    
+    newLI.textContent = users;
+    newUL.append(newLI);
+
+  }
+
+  return newUL;
+
 }
