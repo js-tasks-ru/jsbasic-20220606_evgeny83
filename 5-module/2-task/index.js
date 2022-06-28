9ed413734d79fbaef12e6dc844bf334acb653c6c
OrlyachenkoEvgeny <evgeny.orlyachenko@yandex.ru>
2022-06-28 16:13:41 +0300

    Скрыть элемент по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  document.getElementsByClassName('toggle-text-button')[0].onclick = function() {
+    if (document.getElementById('text').hasAttribute('hidden')) {
+      document.getElementById('text').hidden = false;
+    }
+    else {
+      document.getElementById('text').hidden = true;
+    }
+    
+  };
 }
