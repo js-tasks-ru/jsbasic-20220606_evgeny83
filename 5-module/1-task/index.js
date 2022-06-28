66c23d752b9839083ac7c9683b2865c7841f126b
OrlyachenkoEvgeny <evgeny.orlyachenko@yandex.ru>
2022-06-28 15:26:07 +0300

    Спрятать себя

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,7 @@
 function hideSelf() {
-  // ваш код...
+  const allElem = document.getElementsByClassName('hide-self-button');
+   
+  allElem[0].onclick = function() {
+    allElem[0].hidden = true;
+  };
 }
